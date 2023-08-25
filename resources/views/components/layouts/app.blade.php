27f95e66bfc5cff4a3c9dd16702bc4b93209d6ae
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2023-08-25 18:41:51 +0600

    chore: install tailwindcss

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100 dark:bg-gray-900">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'MailerLite' }}</title>
+
+        @vite('resources/css/app.css')
     </head>
-    <body>
+    <body class="h-full antialiased font-sans">
         {{ $slot }}
     </body>
 </html>
